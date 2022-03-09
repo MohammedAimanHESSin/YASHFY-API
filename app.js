ce9ce0ecbbf51e6c04ec96bbc336a002f9998f85
mohamed <midoaymen@ymail.com>
2022-03-09 18:03:58 +0200

    Adding starting folders

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.use('/', (req,res,next)=>{
+    console.log('Start Server')
+});
+
+app.listen(8080);
